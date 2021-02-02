879e74c8ef10a65769bb8f0529fccba8834b271b
neobats <batton.guy@gmail.com>
2021-02-01 23:44:40 -0600

    Made another hook for the TextInputs.

---
+++ utils/hooks/useTextInput.ts
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export const useTextInput = (
+  initialState: string = ""
+): [string, (text: string) => void] => {
+  const [value, setValue] = useState(initialState);
+
+  return [value, (text) => setValue(text)];
+};
